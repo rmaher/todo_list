f8a1122da657176dbbec53056d96d030ea2dc552
Mike Zelem <mzelem@itriagehealth.com>
2015-07-28 09:59:18 -0600

    adds TodoListController

---
+++ lib/todo_list_controller.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+
+require './lib/todo_list.rb'
+require './lib/task.rb'
+require 'json'
+require 'pry'
+
+class TodoListController
+
+	# your code here
+
+end
+
+if __FILE__ == $0
+	controller = TodoListController.new
+	controller.run
+end
