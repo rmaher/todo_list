5a7128706b491d3b48bd0c90f69e0cda9b202c2f
Rachel Maher <rachelelizabethmaher@gmail.com>
2015-08-13 16:25:16 -0600

    added Controller

--- lib/todo_list_controller.rb
+++ lib/todo_list_controller.rb
@@ -7,7 +7,69 @@ require 'pry'
 
 class TodoListController
 
-	# your code here
+	attr_accessor :todo_list, :tasks,:title, :priority, :complete
+
+	def initialize
+		self.todo_list = TodoList.new
+		@task = Task.new(title, priority)
+	end
+
+	def list
+		puts "\n\n"
+		puts "Listing tasks:"
+		puts "----------------------------------------------------------------------------------------------"
+	    self.todo_list.tasks.each_with_index do |task, i|
+	    	puts "#{i+1} #{task.to_s} #{task.priority}"
+	    end
+	    puts "\n\n"
+	end
+
+	def save(args)
+        args.each do |file|
+        	@todo_list.update(file)
+        	puts "\n\nFile Saved\n\n"
+       	end 
+	end
+
+	def load(args)
+		args.each do |file|
+			File.open(file)
+		end
+	end
+
+	def complete(tasks)
+        @task.complete
+	end
+
+	def add(args)
+		@todo_list.add(Task.new(args.join('')))
+	end
+    
+
+	def run
+		command = nil
+		print "Please enter commmand: "
+		while command != 'exit'
+			command = gets.chomp
+			
+			case command
+		    when 'list'
+		    	@list
+    		when 'add'
+    			self.add(args)
+    		when 'load'
+    			load(args)
+    		when 'complete'
+    			complete(args)
+    		when 'exit'
+    			puts "Exiting"
+			else 
+				puts "Command not found"
+		    end
+		end
+
+	end
+
 
 end
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,7 @@ require 'rspec'
 
 RSpec.configure do |config|
   config.order = 'random'
+  config.color = true
 end
 
 Dir[File.join(File.dirname(__FILE__), "..", "lib" , "**.rb")].each do |f|
